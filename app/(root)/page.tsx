1c2da76c51d30bc975f34e7b1edcfd46b216400e
swapnil1530 <patelswapnil1004@gmail.com>
2024-01-07 12:01:14 +0530

    [ feat ] : leftsidebar add ✨

---
+++ app/(root)/page.tsx
@@ -0,0 +1,41 @@
+import JobFilterSidebar from "@/components/JobLeftSidebar";
+import H1 from "@/components/ui/h1";
+import { JobFilterValues } from "@/lib/validation";
+import Image from "next/image";
+
+interface PageProps {
+  searchParams: {
+    q?: string;
+    type?: string;
+    location?: string;
+    remote?: string;
+    page?: string;
+  };
+}
+
+export default function Home({
+  searchParams: { q, type, location, remote, page },
+}: PageProps) {
+  const filterValues: JobFilterValues = {
+    q,
+    type,
+    location,
+    remote: remote === "true",
+  };
+
+  return (
+    <main className="m-auto my-10 max-w-7xl space-y-10 px-3">
+      <div className="space-y-5 text-center">
+        {/* <H1>{getTitle(filterValues)}</H1> */}
+        <p className="text-muted-foreground">Find your dream job.</p>
+      </div>
+      <section className="flex flex-col gap-4 md:flex-row">
+        <JobFilterSidebar defaultValues={filterValues} />
+        {/* <JobResults
+          filterValues={filterValues}
+          page={page ? parseInt(page) : undefined}
+        /> */}
+      </section>
+    </main>
+  );
+}
